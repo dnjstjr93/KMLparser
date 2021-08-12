25e8b0aebd246bde86b97a1abaf622953a87a593
dnjstjr93 <dnjstjr93@gmail.com>
2021-08-12 22:56:45 +0900

    - edit code

--- main.py
+++ main.py
@@ -1,7 +1,17 @@
 from fastkml import kml
 import json
+import os
 
-KMLFILE = "../../Desktop/test.kml"
+KML_DIR = './KML/'
+
+JSON_DIR = './JSON/'
+if not os.path.isdir(JSON_DIR):
+    os.mkdir(JSON_DIR)
+
+# KMLFILE = KML_DIR + "booyeo.kml"
+KMLFILE = KML_DIR + "hwasung.kml"
+
+file_name = KMLFILE.split('/')[-1].split('.')[0]
 with open(KMLFILE) as f:
     doc = f.read().encode()
 
@@ -30,32 +40,26 @@ umac8_data["gcs"] = "KETI_MUV"
 umac8_data["goto_position"] = ["cancel"]
 umac8_data["system_id"] = 8
 
-count = 0
 for idx in range(1, len(coordinates)):
-    if count < 2:
-        lat = coordinates[idx][1]
-        lon = coordinates[idx][0]
-        alt = coordinates[idx][2]
+    lat = coordinates[idx][1]
+    lon = coordinates[idx][0]
+    alt = coordinates[idx][2]
 
-        data = "%.7f:%.7f:%.1f:%.1f" % (lat, lon, 100.0, 10.0)
-        if alt == 154:  # alttitude = 50 in QGC
-            umac6_data["goto_position"].append(data)
-        elif alt == 204:  # alttitude = 100 in QGC
-            umac7_data["goto_position"].append(data)
-        elif alt == 254:  # alttitude = 150 in QGC
-            umac8_data["goto_position"].append(data)
+    temp_alt = coordinates[1][2]
 
-        count += 1
-    elif count == 2:
-        count += 1
-    else:
-        count = 0
+    data = "%.7f:%.7f:%.1f:%.1f" % (lat, lon, 100.0, 10.0)
+    if alt == temp_alt:  # alttitude = 50 in QGC
+        umac6_data["goto_position"].append(data)
+    elif alt == temp_alt + 50:  # alttitude = 100 in QGC
+        umac7_data["goto_position"].append(data)
+    elif alt == temp_alt + 100:  # alttitude = 150 in QGC
+        umac8_data["goto_position"].append(data)
 
-with open('./UMAC6.json', 'w', encoding='utf-8') as make_file:
+with open(JSON_DIR + file_name + '_UMAC6.json', 'w', encoding='utf-8') as make_file:
     json.dump(umac6_data, make_file, indent="\t")
 
-with open('./UMAC7.json', 'w', encoding='utf-8') as make_file:
+with open(JSON_DIR + file_name + '_UMAC7.json', 'w', encoding='utf-8') as make_file:
     json.dump(umac7_data, make_file, indent="\t")
 
-with open('./UMAC8.json', 'w', encoding='utf-8') as make_file:
+with open(JSON_DIR + file_name + '_UMAC8.json', 'w', encoding='utf-8') as make_file:
     json.dump(umac8_data, make_file, indent="\t")
